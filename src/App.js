db3d0286f704f5a08671f34569784cb5316fd832
Amankaium <kaium@list.ru>
2022-04-22 18:34:38 +0600

    add text React Project

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Hello world!
         </p>
+        <div>
+          React project
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
